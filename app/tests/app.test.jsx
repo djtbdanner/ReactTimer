bb07a89a73b319c6bc296e31a30a0c110ed5877e
- <djtbdanner@yahoo.com>
2017-11-18 08:06:15 -0600

    just another test

--- app/tests/app.test.jsx
+++ app/tests/app.test.jsx
@@ -3,7 +3,11 @@ var expect = require('expect');
 
 
 describe('App', () => {
-  it('should properly run tests', () => {
+  it('Just a little test', () => {
     expect(1).toBe(1);
-  });  
+  });
+
+  it('This is another little test', () => {
+    expect(1).toBe(1);
+  });
 });
