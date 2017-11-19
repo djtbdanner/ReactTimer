f111b84bf3302c941698f32506e43ed576586c42
- <djtbdanner@yahoo.com>
2017-11-19 09:13:32 -0600

    functions stop pause start

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var Clock = require('Clock');
 var CountdownForm = require('CountdownForm') ;
+var Controls = require('Controls');
 
 var Countdown = React.createClass({
 
@@ -11,8 +12,12 @@ var Countdown = React.createClass({
         case "started":
           this.startTimer();
           break;
+        case "stopped":
+          this.setState({count:0}); // no break will do next thing too
+        case "paused":
+          clearInterval(this.timer);
+          break;
         default:
-
       }
     }
   },
@@ -36,12 +41,25 @@ var Countdown = React.createClass({
     this.setState({count: seconds, countdownStatus: "started"});
   },
 
+  handleStatusChange: function(newStatus){
+    this.setState({countdownStatus:newStatus});
+  },
+
   render: function() {
-    var {count} = this.state;
+   var {count, countdownStatus} = this.state;
+
+   var renderControlArea = () => {
+     if (countdownStatus !== 'stopped') {
+       return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
+     } else {
+      return <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+     }
+   };
+
     return (
       <div>
         <Clock totalSeconds={count}/>
-        <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+        {renderControlArea()}
       </div>
     );
   }
