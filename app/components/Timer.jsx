d965320a383afe18c70bb91d0b11f2f85f64253b
- <djtbdanner@yahoo.com>
2017-11-19 10:33:09 -0600

    added timer app

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,59 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
+var CountdownForm = require('CountdownForm') ;
 
-var Timer = (props) => {
-  return (
-    <div>
-        <p>Timer.jsx here</p>
-    </div>
-  );
-}
+var Timer = React.createClass({
+
+  getInitialState : function() {
+    return {count: 0, countdownStatus: "paused"};
+  },
+  handleStatusChange: function(newStatus){
+    this.setState({countdownStatus:newStatus});
+  },
+
+  componentWillUnmount: function(){
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+
+  //name specific - called by ReactDOM
+  componentDidUpdate: function (prevProps, prevState){
+    if (this.state.countdownStatus !== prevState.countdownStatus){
+      switch (this.state.countdownStatus) {
+        case "started":
+          this.startTimer();
+          break;
+        case "stopped":
+          this.setState({count:0}); // no break will do next thing too
+        case "paused":
+          clearInterval(this.timer);
+          break;
+        default:
+      }
+    }
+  },
+
+  startTimer: function(){
+    this.timer = setInterval(
+      () => {
+       var newCount = this.state.count + 1;
+       this.setState({count:newCount>=0?newCount:0});
+      },
+    1000);
+  },
+
+  render: function() {
+    var {count, countdownStatus} = this.state;
+
+    return(
+      <div>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+        <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
+      </div>
+    );
+  }
+});
 
 module.exports = Timer;
---
+++ app/tests/components/Timer.test.jsx
@@ -0,0 +1,31 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Timer = require('Timer');
+
+describe('Timer', () => {
+  it('Should exist', () => {
+    expect(Timer).toExist();
+  });
+
+
+  describe('Handle timer stuff', () => {
+    // add done here because that one test takes a second to run
+    it('Should set state to started and count up', (done) => {
+
+      var timer = TestUtils.renderIntoDocument(<Timer/>);
+      timer.handleStatusChange("started");
+      expect(timer.state.countdownStatus).toBe("started");
+      expect(timer.state.count).toBe(0);
+
+      setTimeout(() => {
+        expect(timer.state.count).toBe(1);
+        done();
+      }, 1001);
+    });
+  });
+
+});
